70a59df1c4a0630c5ba8a1d09e1d10de995db1d8
Dragonashes <dlsfs@tutanota.de>
2021-11-25 14:54:04 +0100

    simplified append

--- assets/js/indexfetch.js
+++ assets/js/indexfetch.js
@@ -13,8 +13,7 @@ fetch('https://jsonplaceholder.typicode.com/users')
       div3.classList.add("jokeitem", "animate")
       // div3.textContent = element.name
       div3.innerHTML = "<img src='assets/image/profile2.svg' class='placearticleitem1'>" + element.name
-      div1.appendChild(div2)
-      div1.appendChild(div3)
+      div1.appendChild(div2, div3)
       document.querySelector("main").appendChild(div1)
     });
   })
